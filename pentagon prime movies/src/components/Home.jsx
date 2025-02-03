303724ee64c04469ed0adccbb748bf25d6adbdf1
Kohmmz <jeremiahkoome1@gmail.com>
2025-02-03 08:17:15 +0300

    refactor: enhance Home and MovieCard components with improved styling and structure

--- pentagon prime movies/src/components/Home.jsx
+++ pentagon prime movies/src/components/Home.jsx
@@ -3,15 +3,15 @@ import { useNavigate } from "react-router-dom";
 export default function Home() {
     const navigate = useNavigate();
     return (
-        <div>
+        <div >
             <h1>PENTAGON PRIME MOVIES</h1>
-            <p>Your prime destination for discovering and exploring the finest movies.</p>
-            <div>
+            <p className="intro"> Your prime destination for discovering and exploring the finest movies.</p>
+            <div className="home-buttons">
                 {/* Navigation buttons for movie browsing and learning more */}
-                <button onClick={() => navigate("/movies")}>Browse Movies</button>
-                <button onClick={() => navigate("/about")}>Learn More</button>
+                <button  className="Browse"  onClick={() => navigate("/movies")}>Browse Movies</button>
+                <button  className= "Learn"   onClick={() => navigate("/about")}>Learn More</button>
             </div>
         </div>
     );
 }
-//Anwar 
+//Anwar
--- pentagon prime movies/src/components/MovieCard.jsx
+++ pentagon prime movies/src/components/MovieCard.jsx
@@ -8,18 +8,33 @@ function MovieCard({ movie }) {
   };
 
   return (
-    <div onClick={handleCardClick}>
-      {showDetails ? (// Show details if showDetails is true
+    <div className="movie-card" onClick={handleCardClick}>
+      {showDetails ? ( // Show details if showDetails is true
         <div>
           <h2>{movie.Title}</h2>
           <img src={movie.Poster} alt={movie.Title} />
           <p><strong>Year:</strong> {movie.Year}</p>
           <p><strong>Genre:</strong> {movie.Genre}</p>
           <p><strong>Director:</strong> {movie.Director}</p>
-          <p>{movie.Plot}</p>
+          <p><strong>Plot:</strong>{movie.Plot}</p>
+          <p><strong>Rated:</strong> {movie.Rated}</p>
+
+          {movie.Ratings && movie.Ratings.length > 0 && (
+            <div>
+              <h4>Ratings:</h4>
+              <ul>
+                {movie.Ratings.map((rating, index) => (
+                  <li key={index}>
+                    <strong>{rating.Source}:</strong> {rating.Value}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+
           <button onClick={() => setShowDetails(false)}>Back to List</button>
         </div>
-      ) : (// Show basic view if showDetails is false
+      ) : ( // Show basic view if showDetails is false
         <div>
           <img src={movie.Poster} alt={movie.Title} style={{ width: "150px" }} />
           <h2>{movie.Title}</h2>
@@ -30,4 +45,3 @@ function MovieCard({ movie }) {
 }
 
 export default MovieCard;
-//Jeremiah
